3df253f762a2930ee2911b3d3fc1124d9a0b2985
hardertoget <davisousanovo@outlook.com>
2024-11-13 00:52:37 -0300

    Trocado o valor em starships.test.js para testar o git actions.

--- tests/starships.test.js
+++ tests/starships.test.js
@@ -4,6 +4,6 @@ describe('Star Wars API - Starships', () => {
   it('should fetch the first starship', async () => {
     const response = await axios.get('https://swapi.dev/api/starships/2/');
     expect(response.status).toBe(200);
-    expect(response.data.name).toBe('Cruiser');
+    expect(response.data.name).toBe('CR90 corvette');
   });
 });
